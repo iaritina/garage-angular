066e796de6ce059fb50dc33e7a7b5cf7ee644e3b
iaritina <iaritina7@gmail.com>
2025-03-11 17:08:28 +0300

    update routes for vehicle regiser

--- src/app/pages/authentication/authentication.routes.ts
+++ src/app/pages/authentication/authentication.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AppSideLoginComponent } from './side-login/side-login.component';
 import { AppSideRegisterComponent } from './side-register/side-register.component';
+import { RegisterClientVehicleComponent } from './register-client-vehicle/register-client-vehicle.component';
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -14,6 +15,10 @@ export const AuthenticationRoutes: Routes = [
       {
         path: 'register',
         component: AppSideRegisterComponent,
+      },
+      {
+        path: 'register-your-vehicle',
+        component: RegisterClientVehicleComponent,
       },
     ],
   },
