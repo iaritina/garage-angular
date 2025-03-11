5b8bbd06cda8a6d0b1c9522aac52f32a5333151a
NyHaroavo <razakamahefaharoavo9@gmail.com>
2025-03-11 23:27:49 +0300

    add, update and delete service and fix error from table

--- src/app/services/vehicle/vehicle.service.ts
+++ src/app/services/vehicle/vehicle.service.ts
@@ -17,4 +17,12 @@ export class VehicleService {
     console.log(value);
     return this.http.post<any>(`${environment.apiUrl}/vehicles/`, value);
   }
+
+  deleteVehicle(id: string) {
+    return this.http.put<any>(`${environment.apiUrl}/vehicles/delete/${id}`,{});
+  }
+
+  updateVehicle(id: string, data: any):Observable<any> {
+    return this.http.patch<any>(`${environment.apiUrl}/vehicles/${id}`,data);
+  }
 }
