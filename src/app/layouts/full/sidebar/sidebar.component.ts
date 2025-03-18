3514d8665c8278346cad156701586156bdc33fb6
iaritina <iaritina7@gmail.com>
2025-03-18 22:22:55 +0300

    fix icons, logo, responsivness

--- src/app/layouts/full/sidebar/sidebar.component.ts
+++ src/app/layouts/full/sidebar/sidebar.component.ts
@@ -14,6 +14,7 @@ import { MaterialModule } from 'src/app/material.module';
   selector: 'app-sidebar',
   imports: [BrandingComponent, TablerIconsModule, MaterialModule],
   templateUrl: './sidebar.component.html',
+  styleUrl: './style.scss',
 })
 export class SidebarComponent implements OnInit {
   constructor() {}
